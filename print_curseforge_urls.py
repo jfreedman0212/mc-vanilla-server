9e0f2ec2d33b2eec17819bcb252ac3125e5affa6
Josh Freedman <josh@freedman.dev>
2024-10-29 09:57:28 -0400

    Make this a vanilla server

--- print_curseforge_urls.py
+++
@@ -1,9 +0,0 @@
-import json;
-
-for f in json.load(open("manifest.json", "r"))["files"]:
-    if not f["required"]:
-        continue
-    file_id = f["fileID"]
-    project_id = f["projectID"]
-    print("https://www.curseforge.com/api/v1/mods/%i/files/%i/download" % (project_id, file_id))
-
